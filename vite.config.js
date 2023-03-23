fc003ef30fb136bdf21cc0c49890e2d855780534
Lykrat <emil.treptow@outlook.com>
2023-03-23 17:41:50 +0100

    fixing the dist

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/dynamisk_cv_hemsida/',
-  plugins: [react()],
-})
